1179c776bd08764d95cd99793c033f7a8d768beb
Margo <12rita43@gmail.com>
2023-01-11 02:00:15 +0300

    task 12 has been done

--- task12.js
+++ task12.js
@@ -1,17 +1,16 @@
-import {data12, example12} from "./inputs/data12.js";
+import {data12} from "./inputs/data12.js";
 
 const map = [];
 
 
-example12.split('\n').forEach((line, y) => {
+data12.split('\n').forEach((line, y) => {
     map.push(line.split('').map((letter, x) => {
-        return {x, y, charCode: letter.charCodeAt(0), optimal: false, vS: Infinity}
+        return {x, y, charCode: letter.charCodeAt(0), visited: false, letter, counter: 0}
     }));
 })
 
 const startMark = {x: 0, y: 0, charCode: 97}
 const endMark = {x: 0, y: 0, charCode: 122}
-let currentPoint = endMark;
 
 map.forEach((line, idx) => {
     const start = line.findIndex((point) => point.charCode === 83);//'S'
@@ -47,114 +46,54 @@ const neighbours = {
 }
 
 
-const counters = [];
 map[endMark.y][endMark.x].charCode = 122;
 map[startMark.y][startMark.x].charCode = 97;
 map[startMark.y][startMark.x].vS = 0;
 
 const queue = [map[endMark.y][endMark.x]];
 
-let counter = 0;
 const getNeighbour = (point, direction) => {
     const {x, y, charCode} = point;
 
     const {x: deltaX, y: deltaY} = neighbours[direction];
 
-    if (map[y + deltaY] && map[y + deltaY][x + deltaX] && !map[y + deltaY][x + deltaX].optimal && charCode - map[y + deltaY][x + deltaX].charCode <= 1) {
+    if (map[y + deltaY] && map[y + deltaY][x + deltaX] && !map[y + deltaY][x + deltaX].visited && charCode - map[y + deltaY][x + deltaX].charCode <= 1) {
         // console.log(map[y + deltaY][x + deltaX])
         return {x: x + deltaX, y: y + deltaY}
     } else return false;
 }
 
-const findMin = (filtered) => {
-    let min = Infinity;
-    let minX = 0;
-    let minY = 0;
-    filtered.forEach((line, y) => {
-        line.forEach((vertex, x) => {
-            if (vertex.vS < min) {
-                min = vertex.vS
-                minX = x;
-                minY = y;
-            }
-        })
 
-    })
-    return {x: minX, y: minY};
-}
-const dijkstraAlg = () => {
-    const filteredMap = [];
-
-    map.forEach(line => {
-        filteredMap.push(line.filter(vertex => !vertex.optimal));
-    })
-    const {x, y} = findMin(filteredMap);
-    const vertex = map[y][x]
-    console.log(vertex)
-    vertex.optimal = true;
-    Object.keys(neighbours).forEach(direction => {
-        const neighbour = getNeighbour(vertex, direction);
-
-        if (neighbour) {
-            map[neighbour.y][neighbour.x].vS = Math.min(map[neighbour.y][neighbour.x].vS, vertex.vS + 1)
-
-        }
-    })
-
-}
-
-const path = () => {
-    let startFlag = false;
+const bfs = () => {
     while (queue.length) {
+        const cell = queue[0];
 
-        queue.forEach(cell => {
-            cell.counters.forEach(counter => counter++);
+        if (!cell.visited) {
+            Object.keys(neighbours).forEach(direction => {
+                const neighbour = getNeighbour(cell, direction);
 
+                if (neighbour) {
 
-            cell.visited = true;
-            if (cell.x === startMark.x && cell.y === startMark.y) {
-                startFlag = true;
-                return false;
-            } else {
+                    map[neighbour.y][neighbour.x].counter = cell.counter + 1;
 
-                Object.keys(neighbours).forEach(direction => {
-                    const neighbour = getNeighbour(cell, direction);
+                    queue.push(map[neighbour.y][neighbour.x])
 
-                    if (neighbour) {
-                        queue.push(map[neighbour.y][neighbour.x])
-                        if (map[neighbour.y][neighbour.x].visited) {
-                            map[neighbour.y][neighbour.x].counters.push(...cell.counters)
-                        }
-                    }
-                })
-
-                console.log({queue})
-                queue.shift();
-                // if ()
-
-
-            }
-
-
-        })
-        if (startFlag) {
-            break;
+                }
+            })
         }
+
+
+        // console.log({cell, queue})
+        cell.visited = true;
+
+        queue.shift();
 
     }
 }
 // path();
 
-const pathD = () => {
-    const unOptimalVertexes = map.some(line => line.some(vertex => !vertex.optimal));
-    // console.log(unOptimalVertexes)
-    const n = map.length;
-    const m = map[0].length;
-    console.log(n*m)
-    for (let i = 0; i < n * m; i++) {
-        dijkstraAlg();
-    }
-
-}
-pathD();
-// console.log(map)
+bfs();
+console.log(map[startMark.y][startMark.x])
+const shortestFromA = map.flat(1).filter(item => item.charCode === 97).sort((a, b) => a.counter - b.counter).find(v => v.counter)
+// console.log(shortestFromA[0], shortestFromA[1])
+console.log(shortestFromA)
