4947f7a8b81391f7f7c5d5a00514828b98423dbe
Goncharova_home <m.goncharova@group-ib.com>
2023-01-13 14:57:16 +0300

    task 13 has been done

--- task13.js
+++ task13.js
@@ -1,14 +1,16 @@
-import {example13} from "./inputs/data13.js";
+import {data13} from "./inputs/data13.js";
 
 
-const inputs = example13.split('\n').filter(item => item).map(item => JSON.parse(item));
+
+const inputs = data13.split('\n').filter(item => item).map(item => JSON.parse(item));
 
 const arrayComparsion = (array1, array2) => {
-    // console.log(array1, array2)
+    // console.log({array1, array2})
     for (let i = 0; i < Math.max(array1.length, array2.length) + 1; i++) {
 
         const left = array1[i];
         const right = array2[i];
+        // console.log({left, right})
 
         if (left === undefined && right !== undefined) {
             return true;
@@ -25,6 +27,7 @@ const arrayComparsion = (array1, array2) => {
                 return false;
             }
 
+
         } else if (Array.isArray(left) && Array.isArray(right)) {
             const recursion = arrayComparsion(left, right);
             if (recursion !== undefined) {
@@ -32,10 +35,16 @@ const arrayComparsion = (array1, array2) => {
             }
         } else {
             if (typeof left === 'number') {
-                return arrayComparsion([left], right)
+                const recursion = arrayComparsion([left], right);
+                if (recursion !== undefined) {
+                    return recursion
+                }
             }
             if (typeof right === 'number') {
-                return arrayComparsion(left, [right])
+                const recursion = arrayComparsion(left, [right])
+                if (recursion !== undefined) {
+                    return recursion
+                }
             } else return undefined
         }
 
@@ -66,22 +75,22 @@ const checkOrder = () => {
 }
 
 
-
-
-
 const getRightOrder = () => {
     inputs.push([[2]], [[6]]);
-    console.log({inputs, order:'first'})
-    inputs.sort((a,b)=>{
-        const result = arrayComparsion(a,b)
-        console.log({a, b, result});
-        return result
-    });
-    const firstDivider = inputs.findIndex((item) => JSON.stringify(item) === `[[2]]`);
-    const secondDivider = inputs.findIndex((item) => JSON.stringify(item) === `[[6]]`);
-    // console.log({inputs, order:'second'})
-}
-// getRightOrder();
 
-arrayComparsion([ [ 1 ], [ 2, 3, 4 ] ], [ 1, 1, 5, 1, 1 ]);
+    inputs.sort((a, b) => {
+        const result = arrayComparsion(a, b)
+        if (result) {
+            return -1
+        } else {
+            return 1
+        }
+
+    });
+    const firstDivider = inputs.findIndex((item) => JSON.stringify(item) === `[[2]]`) + 1;
+    const secondDivider = inputs.findIndex((item) => JSON.stringify(item) === `[[6]]`) + 1;
+    console.log(firstDivider * secondDivider)
+}
+getRightOrder();
+
 
